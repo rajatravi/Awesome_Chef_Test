b3bd183eb4384688de702a4f039d1a145ea5fac7
rajatravi <rajatravi230@gmail.com>
2017-07-07 17:57:12 +0000

    changes

--- category3/vhost.rb
+++ category3/vhost.rb
@@ -1,78 +1,70 @@
-apt_package 'nginx' do
+# For centos 7
+package 'nginx' do
   package_name 'nginx'
-  provider  Chef::Provider::Package::Apt
-  version  '1.4.6-1ubuntu3.7'
- action :install
 end
 
-git 'clone' do
-   repository 'git@github.com:OpsTree/Site.git'
+git '/var/www/Opstree' do
+   repository 'https://github.com/OpsTree/Site.git'
    revision 'opstree'
    action :checkout
-   destination '/home/vagrant/git/opstree'
    user 'root'
+   group 'root'
 end
 
-git 'clone_tech' do
-   repository 'git@github.com:OpsTree/Site.git'
+git '/var/www/Techprimo' do
+   repository 'https://github.com/OpsTree/Site.git'
    revision 'techprimo'
    action :checkout
-   destination '/home/vagrant/git/techprimo'
    user 'root'
+   group 'root'
 end
 
-#file '/var/www/opstree.com/html/index.html' do
-## content '<html>This is opstree.</html>'
-## mode '0755'
-##end
-##file '/var/www/techprimo.com/html/index.html' do
-## content '<html>This is techprimo.</html>'
-## mode '0755'
-##end
-##bash 'copy file' do
-##   'sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/opstree.com'
-# #  'sudo cp /etc/nginx/sites-available/opstree.com /etc/nginx/sites-available/techprimo.com'
-# #end
+directory '/etc/nginx/sites-available' do
+  action :create
+end
+
+directory '/etc/nginx/sites-enabled' do
+  action :create
+end
 
 file '/etc/nginx/sites-available/opstree.conf' do
   content 'server {
-listen 80;
-       listen [::]:80;
+    listen  80;
 
-       root /home/vagrant/git/opstree;
-       index index.html index.htm index.nginx-debian.html;
+    server_name opstree.com www.opstree.com;
 
-       server_name opstree.com www.opstree.com;
+    location / {
+        root  /var/www/Opstree;
+        index  index.html index.htm;
+        try_files $uri $uri/ =404;
+    }
 
-       location / {
-               try_files $uri $uri/ =404;
-       }
 }'
 end
 
 file '/etc/nginx/sites-available/techprimo.conf' do
   content 'server {
-       listen 80;
-       listen [::]:80;
-       root  /home/vagrant/git/techprimo;
-       index index.html index.htm index.nginx-debian.html;
+    listen  80;
 
-       server_name techprimo.com www.techprimo.com;
+    server_name techprimo.com www.techprimo.com;
 
-       location / {
-               try_files $uri $uri/ =404;
-       }
+    location / {
+        root  /var/www/Opstree;
+        index  index.html index.htm;
+        try_files $uri $uri/ =404;
+    }
+
 }'
 end
 
 link '/etc/nginx/sites-available/opstree.conf' do
  to '/etc/nginx/sites-enabled/opstree.conf'
-link_type :symbolic
+ link_type :symbolic
 end
 
 link '/etc/nginx/sites-available/techprimo.conf' do
  to '/etc/nginx/sites-enabled/techprimo.conf'
-link_type :symbolic
+ link_type :symbolic
 end
 
 service 'nginx' do

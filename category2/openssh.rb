cefd04f5c04f02e0b6a830e28defe4f79b4626f1
rajatravi <rajatravi230@gmail.com>
2017-07-10 05:48:21 +0000

    changes done in category2

--- category2/openssh.rb
+++ category2/openssh.rb
@@ -3,7 +3,7 @@ describe sshd_config ('/etc/ssh/sshd_config') do
 end
 bash 'generate_SSH_Key' do  
 code <<- SSH  
- sh-keygen -t rsa 
+ ssh-keygen -t rsa 
  ssh-copy-id localhost            
  chmod 600 .ssh/authorized_keys         
  SSH 
